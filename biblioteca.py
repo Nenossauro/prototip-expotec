9471de8ddd3fa68fafe8b52d31b09440ace0891b
Nenossauro <samuelperes.arroba@gmail.com>
2023-08-04 22:46:02 -0300

    Quatro de agosto

--- biblioteca.py
+++ biblioteca.py
@@ -10,6 +10,17 @@ app.secret_key = 'enzo'
 os.system("cls")
 #Executando o comando "cls" para limpar a tela do console.
 
+class Usuario:
+    def __init__(self,nome,genero,idade,estado,id):
+        self.nome = nome
+        self.genero = genero
+        self.idade = idade
+        self.estado = estado
+        self.id = id
+
+        
+
+
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["biblioteca"]
 collection = db["users"]
@@ -32,47 +43,106 @@ def inicio():
     else:
         meu_cookie = '0'
     livros_id = collection_infos.find()
-    generos = []
-    ids = []
-    nomes = []
-    estados = []
-    idades = []
+    users = []
     for item in livros_id:
         v_nome = item["nome"]
         v_id = item["id"]
         v_genero = item["genero"]
         v_estado = item["estado"]
         v_idades = item["idade"]
-        nomes.append(v_nome)
-        generos.append(v_genero)
-        estados.append(v_estado)
-        idades.append(v_idades)
-        ids.append(int(v_id))
+        usuario = Usuario(nome=v_nome, genero=v_genero, idade=v_idades, estado=v_estado, id=int(v_id))
+        users.append(usuario)
+    idadesp = ["18","17","16","15"]
+    
 
-    data_labels = []
-    for id in range(len(idades)):
-        data_labels.append(id)
-
-
-    df = pandas.DataFrame({'idade':idades, 'label':data_labels})
-
-
-    grafico_altair = altair.Chart(df).mark_arc(size=200).encode(
+    idades = []
+    for ages in users:
+       t_ages = ages.idade
+       idades.append(t_ages)
+    relacao = []
+    dzot = 0
+    dzst = 0
+    dzss = 0
+    qnze = 0  
+    for usuario in users:
+        if usuario.idade == "18":
+            dzot += 1
+        elif usuario.idade == "17":
+            dzst += 1
+        elif usuario.idade == "16":
+            dzss += 1
+        else:
+            qnze += 1
+    relacao.append(dzot)
+    relacao.append(dzst)
+    relacao.append(dzss)
+    relacao.append(qnze)
+    total_usuarios = len(users)
+    df = pandas.DataFrame({'idade':relacao, 'Idades':idadesp})
+    df['porcentagem'] = round((df['idade'] / total_usuarios) * 100)
+    print("Quantidade de cada idade:",relacao)
+    print("Idades possiveis:",idadesp)
+    grafico_altair_idade = altair.Chart(df).mark_arc(size=100).encode(
         theta='idade:Q',
-        color='idade:O',
-        tooltip='idade:Q'
+        color='Idades:O',
+        tooltip='porcentagem:N'
         
     ).properties(
-        width=400,
-        height=300,
-        title='Distribuição de Idade dos Usuários'
-    ).interactive()
+        width=200,
+        height=150,
+        title='Distribuição de Idade dos Usuários em %'
+    )
+
+
+
+    fem = 0
+    masc = 0
+    nb = 0
+    relgen = []
+    generos = ["Masculino","Feminino","Não Binário"]
+
+    for usuario in users:
+        if usuario.genero == "Masculino":
+            masc += 1
+        elif usuario.genero == "Feminino":
+            fem += 1
+        else:
+            nb += 1
+    relgen.append(masc) 
+    relgen.append(fem)
+    relgen.append(nb)
+    print("Quantidade de cada genero:",relgen)
+    print("Generos possiveis:",generos)
+    dfg = pandas.DataFrame({'Genero':relgen,'Generos':generos})
+    dfg['porcentagem'] = round((dfg['Genero'] / total_usuarios) * 100)
+    
+    grafico_altair_genero = altair.Chart(dfg).mark_arc(size=100).encode(
+        theta='Genero:Q',
+        color='Generos:O',
+        tooltip='porcentagem:N'
+        
+    ).properties(
+        width=200,
+        height=150,
+        title='Distribuição de genero dos Usuários em %'
+    )
+
+    
 
 
 
 
 
-    return render_template('land.html',pessoa =session['user_logged'],nome = nomes,ids = ids,genero=generos,id_liv = int(meu_cookie),estado = estados, idade = idades, grafico_altair=grafico_altair.to_html())
+
+
+
+
+
+    grafico_altair_idade_json = grafico_altair_idade.to_json()
+    grafico_altair_genero_json = grafico_altair_genero.to_json()
+
+
+    return render_template('land.html',pessoa =session['user_logged'], id_liv = int(meu_cookie), grafico_altair=grafico_altair_idade_json,grafico_altair_genero=grafico_altair_genero_json)
 #Definindo a rota '/inicio', que requer autenticação. 
 #Se o usuário não estiver logado, será redirecionado para a página principal ('/'). 
 #Caso contrário, o template 'land.html' será renderizado, passando a variável 'pessoa' com o valor da sessão 'user_logged'.
--- teste.py
+++ teste.py
@@ -4,6 +4,18 @@ import altair as alt
 import pandas as pd
 #Importando os módulos necessários para o projeto Flask.
 
+class Usuario:
+    def __init__(self,nome,genero,idade,estado,id):
+        self.nome = nome
+        self.genero = genero
+        self.idade = idade
+        self.estado = estado
+        self.id = id
+
+def espaco(quantos):
+    for q in range(quantos):
+        print("")
+
 app = Flask(__name__)
 app.secret_key = 'enzo'
 #Criando uma instância do objeto Flask e definindo a chave secreta para uso nas sessões.
@@ -22,22 +34,106 @@ ids = []
 nomes = []
 estados = []
 idades = []
+users = []
 for item in livros_id:
     v_nome = item["nome"]
     v_id = item["id"]
     v_genero = item["genero"]
     v_estado = item["estado"]
     v_idades = item["idade"]
-    nomes.append(v_nome)
-    generos.append(v_genero)
-    estados.append(v_estado)
-    idades.append(v_idades)
-    ids.append(int(v_id))
 
-idade_data = [20, 30, 25, 35, 40]
+    usuario = Usuario(nome=v_nome, genero=v_genero, idade=v_idades, estado=v_estado, id=int(v_id))
+
+    users.append(usuario)
+
+
+
+
+print("O que você quer saber?")
+print("1. Quantas usuarios tem o site?")
+print("2. Qual a proporção de gênero dos usuarios?")
+print("3. Qual estado os usuarios moram?")
+print("4. Qual a idade de nossos usuarios?")
+espaco(1)
+
+op = int(input())
+match(op):
+    case 1:
+        qtd = 0
+        for usuario in users:
+            qtd += 1
+
+        print("Temos {} usuarios em nosso site!".format(qtd))
+        
+    case 2:
+        qtdm = 0
+        qtdf = 0
+        qtdnb = 0
+        for usuario in users:
+            if usuario.genero == "Masculino":
+                qtdm += 1
+            elif usuario.genero == "Feminino":
+                qtdf += 1
+            else:
+                qtdnb += 1
+        espaco(2)
+        print("Temos {} usuarios do genero masculino, {} usuarios do genero feminino e {} de gênero não binario!".format(qtdm,qtdf,qtdnb))
+        espaco(2)
+        pctm = (qtdm*100)/len(users)
+        pctf = (qtdf*100)/len(users)
+        pctnb = (qtdnb*100)/len(users)
+        print("Isso corresponde a mais ou menos:")
+        print("{}% Masculinos".format(pctm))
+        print("{}% Femininos".format(pctf))
+        print("{}% Não binários".format(pctnb))
+
+    case 3:
+        qtdm = 0
+        for usuario in users:
+            if usuario.estado == "São Paulo":
+                qtdm += 1
+        espaco(1)
+        print("Temos {} usuarios morando em São Paulo e só!".format(qtdm))
+        espaco(1)
+        pctm = (qtdm*100)/len(users)
+        print("Isso corresponde a mais ou menos:")
+        print("{}% São Paulo".format(pctm))
+        print("0% Outros")
+        espaco(1)
+    case 4:
+        dzot = 0
+        dzst = 0
+        dzss = 0
+        qnze = 0  
+        for usuario in users:
+            if usuario.idade == "18":
+                dzot += 1
+            elif usuario.idade == "17":
+                dzst += 1
+            elif usuario.idade == "16":
+                dzss += 1
+            else:
+                qnze += 1
+        espaco(1)
+        print("Temos {} usuarios de 18 anos, {} usuarios de 17 anos, {} usuarios de 16 anos e {} usuarios de 15 anos!".format(dzot,dzst,dzss,qnze))
+        espaco(1)
+        pdoito = (dzot*100)/len(users)
+        pdsete = (dzst*100)/len(users)
+        pdseis = (dzss*100)/len(users)
+        pquinz = (qnze*100)/len(users)
+        print("Isso corresponde a mais ou menos:")
+        print("{}% 18".format(round(pdoito)))
+        print("{}% 17".format(round(pdsete)))
+        print("{}% 16".format(round(pdseis)))
+        print("{}% 15".format(round(pquinz)))
+        espaco(1) 
+
+
+
+"""idade_data = [20, 30, 25, 35, 40]
 idade_labels = ['20-24', '25-29', '30-34', '35-39', '40+']
 
-# Criação do DataFrame com os dados
+ Criação do DataFrame com os dados
 data = pd.DataFrame({'idade': idade_data, 'label': idade_labels})
 
 
@@ -53,4 +149,4 @@ grafico_altair = alt.Chart(data).mark_circle(size=200).encode(
     ).interactive()
 
 
-grafico_altair.show()
+grafico_altair.show()"""
